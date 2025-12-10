73881b6838245f6db038ad1c45f81ad10a18ac8e
ItBeeZ <balintrichard-20604@taszi.hu>
2025-12-10 15:59:52 +0100

    Adattábla létrehozva és normalizálva

--- asztali/asztali/Model/ContactItem.cs
+++ asztali/asztali/Model/ContactItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace asztali.Model
 {
-    internal class ContactItem
+    [Table("Contacts")]
+    public class ContactItem
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
     }
 }
